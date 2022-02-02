0d50dc8e062452ee2eef0a1bb5474eb6184ff358
john <izolo@mail.com>
2022-02-02 12:36:21 +0300

    add locators.py:         LoginPageLocators()             add LOGIN_EMAIL             add LOGIN_PASSWORD             add REG_EMAIL             add REG_PASSWORD             add REG_PASSWORD_CONFIRM
    add login_page.py
           add should_be_login_url
           add should_be_login_form
           add should_be_register_form

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+
+    #ВХОД
+    LOGIN_EMAIL = (By.ID, "id_login-username")
+    LOGIN_PASSWORD = (By.ID, "id_login-password")
+
+    #РЕГИСТРАЦИЯ
+    REG_EMAIL = (By.ID, "id_registration-email")
+    REG_PASSWORD = (By.ID, "id_registration-password1")
+    REG_PASSWORD_CONFIRM = (By.ID, "id_registration-password2")
--- pages/main_page.py
+++ pages/main_page.py
@@ -6,10 +6,10 @@ from .base_page import BasePage
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        assert self.is_element_present(
-            *MainPageLocators.LOGIN_LINK), \
-            "Login link is not presented"
-
+        login_link = self.is_element_present(
+            *MainPageLocators.LOGIN_LINK)
+        assert login_link.click(), \
+            "Не удается перейти на стр. Login"
 
     def should_be_login_link(self):
         assert self.is_element_present(
