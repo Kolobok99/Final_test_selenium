44bac8880c86a0bd00c70e3f7157f97ca36cf9a2
john <izolo@mail.com>
2022-02-03 20:05:27 +0300

    Finall Commit and Push (5) add README.md
    add locators.py
    почему то удалились:-(

--- pages/locators.py
+++ pages/locators.py
@@ -4,16 +4,18 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators():
 
-    #ВХОД
+class LoginPageLocators():
+    # ВХОД
     LOGIN_EMAIL = (By.ID, "id_login-username")
     LOGIN_PASSWORD = (By.ID, "id_login-password")
 
-    #РЕГИСТРАЦИЯ
+    # РЕГИСТРАЦИЯ
     REG_EMAIL = (By.ID, "id_registration-email")
     REG_PASSWORD = (By.ID, "id_registration-password1")
     REG_PASSWORD_CONFIRM = (By.ID, "id_registration-password2")
+
+    REG_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
 
 
 class ProductPageLocators():
@@ -23,3 +25,16 @@ class ProductPageLocators():
     IN_STOCK = (By.CSS_SELECTOR, ".instock.availability")
     IN_STOCK_MESSAGE = (By.CSS_SELECTOR, "[class='alert alert-safe alert-noicon alert-success  fade in']:nth-child(1)")
     TOTAL_PRICE_MESSAGE = (By.CSS_SELECTOR, "[class='alertinner '] >p > strong")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+    CART_LINK = (By.CSS_SELECTOR, ".btn-group > a")
+
+    USER_ICON = (By.CLASS_NAME, "icon-user")
+
+
+class CartPageLocators():
+    CART_EMPTY = (By.CSS_SELECTOR, "#content_inner > p")
